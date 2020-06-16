17ae9329c6458524ac729ed6fd76a9c80174aa19
emanuele <emanuele.scalone@gmail.com>
2020-06-16 11:30:04 +0200

    Test FF (change in folders)

--- write_output.py
+++ write_output.py
@@ -1,6 +1,7 @@
 def write_atomtypes_atp(pep_atomtypes):
     # This function is used to create the atomtypes.atp.
-    file = open("output/atomtypes.atp", "w")
+    file = open("../../magros_test/commons/output/atomtypes.atp", "w")
+    #file = open("output/atomtypes.atp", "w")
     file.write("[ atomtypes ]")
     file.write("\n")
     file.write(str(pep_atomtypes.to_string(index = False, header = False)))
@@ -9,7 +10,8 @@ def write_atomtypes_atp(pep_atomtypes):
 
 def write_gromos_topology(gromos_topology):
     # This function creates the atomtypes file to paste into the custom topology
-    file = open("output/topology_gromos", "w")
+    file = open("../../magros_test/commons/output/topology_gromos", "w")
+    #file = open("output/topology_gromos", "w")
     file.write("[ atoms ]")
     file.write("\n")
     file.write(str(gromos_topology.to_string(index = False)))
@@ -18,14 +20,16 @@ def write_gromos_topology(gromos_topology):
 
 def write_smog_to_gromos_dihedrals(propers_to_gro):
     # This function creates the angles file to paste into the topology
-    file = open("output/smog_to_gromos_dihedrals", "w")
+    file = open("../../magros_test/commons/output/smog_to_gromos_dihedrals", "w")
+    #file = open("output/smog_to_gromos_dihedrals", "w")
     file.write("[ dihedrals ]")
     file.write("\n")
     file.write(str(propers_to_gro.to_string(index = False)))
     file.close()
 
 def write_merge_ffnonbonded(atomtypes, merge_pairs):
-    file = open("output/ffnonbonded.itp", "w")
+    file = open("../../magros_test/commons/output/ffnonbonded.itp", "w")
+    #file = open("output/ffnonbonded.itp", "w")
     file.write("[ atomtypes ]\n")
     file.write(str(atomtypes.to_string(index = False)))
     file.write("\n")
