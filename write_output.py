aecfaf5b0c2d9f08390956168401ae37d7cbfd19
emanuele <emanuele.scalone@gmail.com>
2020-07-22 09:56:56 +0200

    head position

--- write_output.py
+++ write_output.py
@@ -1,5 +1,5 @@
 from atomtypes_definitions import n
-head = "; made using MAGROS.FF script by Emanuele Scalone at Camilloni Lab"
+head = "; Made using MAGROS.FF script by Emanuele Scalone at Camilloni Lab"
 
 
 def write_atomtypes_atp(pep_atomtypes):
@@ -30,11 +30,11 @@ def write_smog_to_gromos_dihedrals(propers_to_gro):
     # This function creates the angles file to paste into the topology
     #file = open("../../magros_test/commons/output/smog_to_gromos_dihedrals", "w")
     file = open("output/smog_to_gromos_dihedrals", "w")
-    file.write("[ dihedrals ]")
-    file.write("\n")
     file.write(str(head))
     file.write("\n")
     file.write(f'; Temperature Ratio: {n} / 70')
+    file.write("\n")
+    file.write("[ dihedrals ]")
     file.write("\n")
     file.write(str(propers_to_gro.to_string(index = False)))
     file.close()
@@ -42,11 +42,11 @@ def write_smog_to_gromos_dihedrals(propers_to_gro):
 def write_merge_ffnonbonded(atomtypes, merge_pairs):
     #file = open("../../magros_test/commons/output/ffnonbonded.itp", "w")
     file = open("output/ffnonbonded.itp", "w")
-    file.write("[ atomtypes ]\n")
     file.write(str(head))
     file.write("\n")
     file.write(f'; Temperature Ratio: {n} / 70')
     file.write("\n")
+    file.write("[ atomtypes ]\n")
     file.write(str(atomtypes.to_string(index = False)))
     file.write("\n")
     file.write("\n")
@@ -57,14 +57,13 @@ def write_merge_ffnonbonded(atomtypes, merge_pairs):
 def write_acid_ffnonbonded(atomtypes, acid_pairs):
     #file = open("../../magros_test/commons/output/ffnonbonded.itp", "w")
     file = open("output/acid_ffnonbonded.itp", "w")
-    file.write("[ atomtypes ]")
-    file.write("\n")
     file.write(str(head))
     file.write("\n")
     file.write(f'; Temperature Ratio: {n} / 70')
     file.write("\n")
     file.write("; ACID PAIRS")
     file.write("\n")
+    file.write("[ atomtypes ]\n")
     file.write(str(atomtypes.to_string(index = False)))
     file.write("\n")
     file.write("\n")
