d770c895a6c2cb476f1528c560a2e921081328d1
emanuele <emanuele.scalone@gmail.com>
2020-12-21 11:54:20 +0100

    B2m test with correct chains

--- loop.py
+++ loop.py
@@ -4,8 +4,8 @@ import string
 printable = list(string.printable)
 #print(printable)
 #z = list(map(chr, range(0, 64)))
-for h in printable[:64]:
-    for i in range(0, 85):
+for h in printable[:8]: # number of chains
+    for i in range(0, 538):# 0 to atoms in chain
         print(h)
 
 #print(len(string.printable))
