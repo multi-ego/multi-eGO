0f5fe83610d1b0e2d477442858363b67380f123c
emanuele <emanuele.scalone@gmail.com>
2021-02-18 18:10:31 +0100

    remove gromologist files. TODO renumber resids

--- gromologist_fromhome/__init__.py
+++
@@ -1,7 +0,0 @@
-from gromologist.Topology import Top
-from gromologist.Section import Section, SectionMol, SectionParam
-from gromologist.Subsection import Subsection, SubsectionAtom, SubsectionBonded, SubsectionHeader, SubsectionParam
-from gromologist.Entries import Entry, EntryAtom, EntryBonded, EntryParam
-from gromologist.Pdb import Pdb, Atom, Traj
-from gromologist.DihOpt import DihOpt
-from gromologist.Parser import SelectionParser
--- read_topology.py
+++ read_topology.py
@@ -1,17 +1,8 @@
 import gromologist
 from read_input import read_top
-from gromologist import Top, Pdb
 
 
-
-
-print(gromologist.__file__)
 topology = read_top()
 
 protein = topology.molecules[0]
-bonds = protein.list_bonds(by_types=True)
-print(bonds)
-
-
-
-#prova
+protein.list_bonds(by_resid=True)
