82d6e1de71a5fd0385cd480641ca2caaaf97a58a
emanuele <emanuele.scalone@gmail.com>
2021-05-12 11:55:28 +0200

    new dihedrals amylin test

--- protein_configuration.py
+++ protein_configuration.py
@@ -2,18 +2,18 @@
 # In this case there will be TTR and B2m parameters but it is possible to apply them
 # theoretically always
 
-protein = 'TTR'
-#protein = 'Amylin'
+#protein = 'TTR'
+protein = 'Amylin'
 #protein = 'B2m'
 #protein = 'harp'
 
 distance_cutoff = 5.5
 distance_residue = 3
-epsilon_input = 0.320
+epsilon_input = 0.450
 
 # This option requires a long simulation using explicit solvent and a first run of monomer_pairs.py
 # It will make a list of pairs to reweight their epsilon and add to the full force field
-idp = True
+idp = False
 ratio_treshold = 0.09
 
 # Does the N_terminal have a protonation?
