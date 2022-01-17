203b2ae9e4a9498c8d3031c01f49c5b28e09e798
Carlo Camilloni <carlo@CarloMB.local>
2022-01-18 00:34:48 +0100

    fixed a few bugs around:
    - issue with duplication in make_pairs (big!)
    - issue with sorting in merge (medium)
    - other stuff around (small)
    
    the final result is that now the fibril is very stable

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-from os import getresgid
+#from os import getresgid
 from read_input import read_pdbs
 from write_output import write_greta_LJ, write_greta_atomtypes_atp, write_greta_topology_atoms, write_greta_topology_pairs
 from greta import make_pairs_exclusion_topology, make_pairs, merge_GRETA, make_pdb_atomtypes, make_idp_epsilon
 from topology_definitions import acid_atp, first_resid
 from protein_configuration import idp, N_terminal, greta_to_keep, acid_ff
 from mdmat import atomic_mat_plainMD, atomic_mat_random_coil
-
-
 
 print('\n\n\n\n GRETA\n\n\n\n')
 print('GRETA - PDB reading')
@@ -17,13 +15,11 @@ native_pdb, fibril_pdb = read_pdbs()
 
 print('GRETA - Making Atomtypes')
 native_atomtypes, fibril_atomtypes, ffnonbonded_atp, atomtypes_atp, topology_atoms, type_c12_dict, proline_n = make_pdb_atomtypes(native_pdb, fibril_pdb)
-native_atomtypes = (sorted(native_atomtypes))
 
 print('\n GRETA - Making native and fibril pairs')
 
 if greta_to_keep == 'native':
     if idp == True:
-        #greta_LJ = make_idp_epsilon()
         greta_LJ = make_idp_epsilon(atomic_mat_plainMD, atomic_mat_random_coil)
         check = set(greta_LJ['ai'].to_list() + greta_LJ['aj'].to_list())
         check = (sorted(check))
@@ -33,9 +29,8 @@ if greta_to_keep == 'native':
                 greta_LJ = greta_LJ[~greta_LJ.ai.isin(acid_atp)]
                 greta_LJ = greta_LJ[~greta_LJ.aj.isin(acid_atp)]
 
-
 elif greta_to_keep == 'fibril':
-    greta_LJ = make_pairs(fibril_pdb, fibril_atomtypes)
+    greta_LJ = make_pairs(fibril_pdb, atomic_mat_random_coil, fibril_atomtypes)
 
 elif greta_to_keep == 'all':
     if idp == True:
@@ -43,24 +38,15 @@ elif greta_to_keep == 'all':
         greta_LJ = make_idp_epsilon(atomic_mat_plainMD, atomic_mat_random_coil).append(make_pairs(structure_pdb=fibril_pdb, atomic_mat_random_coil=atomic_mat_random_coil, atomtypes=fibril_atomtypes), sort = False, ignore_index = True)
     else:
         # Merging native and fibril LJ pairs.
-        greta_LJ = make_pairs(native_pdb, native_atomtypes)
+        greta_LJ = make_pairs(native_pdb, atomic_mat_random_coil, native_atomtypes)
         if acid_ff == True and acid_atp !=0:
                 greta_LJ = greta_LJ[~greta_LJ.ai.isin(acid_atp)]
                 greta_LJ = greta_LJ[~greta_LJ.aj.isin(acid_atp)]
-        greta_LJ.append(make_pairs(fibril_pdb, fibril_atomtypes), sort = False, ignore_index = True)
+        greta_LJ.append(make_pairs(fibril_pdb, atomic_mat_random_coil, fibril_atomtypes), sort = False, ignore_index = True)
 
 else:
     print('ERRORONE')
     exit()
-
-
-#if native_atomtypes == check:
-#    print('same')
-#else:
-#    print('Check names')
-#    #exit()
-
-    
 
 print('\n GRETA - native and fibril pairs creation completed')
 print('\n GRETA - Merging native and fibril pairs')
@@ -76,31 +62,12 @@ topology_pairs, topology_exclusion = make_pairs_exclusion_topology(greta_ffnb, t
 write_greta_topology_pairs(topology_pairs, topology_exclusion)
 print('\n GRETA - Pairs and Exclusion section written')
 
-
 if N_terminal == True:
     print('Removing N_1 N_1 pair')
     greta_ffnb.loc[(greta_ffnb['; ai'] == first_resid) & (greta_ffnb['aj'] == first_resid), '; ai'] = ';'+greta_ffnb['; ai'] 
 
 write_greta_LJ(ffnonbonded_atp, greta_ffnb)
 
-
 print('\n GRETA - FF Written. Change the masses and copy ffnonbonded.itp and atomtypes.atp into the ff folder.')
-
-
-## Analysis outputs
-#print('Writing Force Field Analysis pairs')
-#ff_name = 'native'
-#if idp == False:
-#    write_pairs_list(native_pdb_pairs, ff_name)
-#else:
-#    write_pairs_list(native_new, ff_name)
-#
-#ff_name = 'fibril'
-#write_pairs_list(fibril_pdb_pairs, ff_name)
-#ff_name = 'merge'
-#write_pairs_list(greta_ffnb, ff_name)
-
-
-
 
 print('GRETA complete! Carlo is happy')
