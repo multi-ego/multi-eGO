efb64437f6c5ff05bc78130242cf2fc956e49bdf
Carlo Camilloni <carlo@CarloMB.local>
2022-02-04 00:45:36 +0100

    from native-fibril-merge to single-merge-rc

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import sys, getopt
 from topology_definitions import raw_top 
 from read_input import read_pdbs, plainMD_mdmat, random_coil_mdmat
 from write_output import write_greta_LJ, write_greta_atomtypes_atp, write_greta_topology_atoms, write_greta_topology_pairs
-from greta import make_pairs_exclusion_topology, make_pairs, merge_GRETA, make_pdb_atomtypes, make_idp_epsilon
+from greta import make_pairs_exclusion_topology, make_pairs, merge_GRETA, make_pdb_atomtypes, make_more_atomtypes, make_idp_epsilon
 
 
 #TODO unire RC a greta to keep
@@ -31,24 +31,21 @@ def main(argv):
 
     print('\n\nMulti-eGO (codename: GRETA)\n')
 
-    make_random_coil = False
     idp = False
 
     try:
-        opts, args = getopt.getopt(argv,"hrip:b:e:",["protein=","build-from=","random-coil","epsilon=","ensemble"])
+        opts, args = getopt.getopt(argv,"hip:b:e:",["protein=","build-from=","epsilon=","ensemble"])
     except getopt.GetoptError:
-        print('main.py -p <protein> -b <native|fibril|merge>' )
+        print('main.py -p <protein> -b <single|merge|rc>' )
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
-            print('main.py -p <protein> -b <native|fibril|merge>')
+            print('main.py -p <protein> -b <single|merge|rc>')
             sys.exit()
         elif opt in ("-p", "--protein"):
             parameters['protein'] = arg
         elif opt in ("-b", "--build-from"):
             parameters['greta_to_keep'] = arg
-        elif opt in ("r", "--random-coil"):
-            make_random_coil = True
         elif opt in ("e", "--epsilon"):
             global epsilon_input
             parameters['epsilon_input'] = float(arg)
@@ -72,11 +69,14 @@ def main(argv):
     print('- reading TOPOLOGY')
     raw_topology_atoms, first_resid, acid_atp, topology_bonds, atom_topology_num = raw_top(parameters)
     print('- reading PDB')
-    # TODO togli la lettura della fibrilla nel caso fibrilla -> *ARG
-    native_pdb, fibril_pdb = read_pdbs(parameters)
+    native_pdb = read_pdbs(parameters, False)
+    if parameters['greta_to_keep'] == 'merge':
+        fibril_pdb = read_pdbs(parameters, True)
 
     print('- Generating Atomtypes')
-    native_atomtypes, fibril_atomtypes, ffnonbonded_atp, atomtypes_atp, topology_atoms, type_c12_dict, proline_n = make_pdb_atomtypes(native_pdb, fibril_pdb, raw_topology_atoms)
+    native_atomtypes, ffnonbonded_atp, atomtypes_atp, topology_atoms, type_c12_dict, proline_n = make_pdb_atomtypes(native_pdb, raw_topology_atoms)
+    if parameters['greta_to_keep'] == 'merge':
+        fibril_atomtypes = make_more_atomtypes(fibril_pdb)
 
     # TODO correggi la massa dell'azoto in prolina
     write_greta_atomtypes_atp(atomtypes_atp, parameters, output_directory)
@@ -84,50 +84,44 @@ def main(argv):
 
     print('- Generating LJ Interactions')
 
-    if make_random_coil == True:
+    if parameters['greta_to_keep'] == 'rc':
         greta_ffnb = pd.DataFrame(columns=['; ai', 'aj', 'type', 'c6', 'c12', '', 'sigma', 'epsilon'])
         write_greta_LJ(ffnonbonded_atp, greta_ffnb, acid_atp, parameters, output_directory)
+        print('- Generating Pairs and Exclusions')
         topology_pairs, topology_exclusion = make_pairs_exclusion_topology(type_c12_dict, proline_n, raw_topology_atoms, topology_bonds, atom_topology_num, parameters)
         write_greta_topology_pairs(topology_pairs, topology_exclusion, parameters, output_directory)
 
-    else:
-        if parameters['greta_to_keep'] == 'native':
-            if idp == True:
-                atomic_mat_plainMD = plainMD_mdmat(parameters)
-                atomic_mat_random_coil = random_coil_mdmat(parameters)
-                greta_LJ = make_idp_epsilon(atomic_mat_plainMD, atomic_mat_random_coil, parameters)
-                check = set(greta_LJ['ai'].to_list() + greta_LJ['aj'].to_list())
-                check = (sorted(check))
-            else:
-                atomic_mat_random_coil = random_coil_mdmat(parameters)
-                greta_LJ = make_pairs(native_pdb, atomic_mat_random_coil, native_atomtypes, parameters)
-                if parameters['acid_ff'] == True and acid_atp !=0:
-                        greta_LJ = greta_LJ[~greta_LJ.ai.isin(acid_atp)]
-                        greta_LJ = greta_LJ[~greta_LJ.aj.isin(acid_atp)]
-
-        elif parameters['greta_to_keep'] == 'fibril':
+    elif parameters['greta_to_keep'] == 'single':
+        if idp == True:
+            atomic_mat_plainMD = plainMD_mdmat(parameters)
             atomic_mat_random_coil = random_coil_mdmat(parameters)
-            greta_LJ = make_pairs(fibril_pdb, atomic_mat_random_coil, fibril_atomtypes, parameters)
-
-        elif parameters['greta_to_keep'] == 'merge':
-            if idp == True:
-                atomic_mat_plainMD = plainMD_mdmat(parameters)
-                atomic_mat_random_coil = random_coil_mdmat(parameters)
-                # Contacts are from a plain MD, so at this step we just import the fibril contacts.
-                greta_LJ = make_idp_epsilon(atomic_mat_plainMD, atomic_mat_random_coil, parameters).append(make_pairs(structure_pdb=fibril_pdb, atomic_mat_random_coil=atomic_mat_random_coil, atomtypes=fibril_atomtypes, parameters=parameters), sort = False, ignore_index = True)
-            else:
-                atomic_mat_random_coil = random_coil_mdmat(parameters)
-                # Merging native and fibril LJ pairs.
-                greta_LJ = make_pairs(native_pdb, atomic_mat_random_coil, native_atomtypes, parameters)
-                if parameters['acid_ff'] == True and acid_atp !=0:
-                        greta_LJ = greta_LJ[~greta_LJ.ai.isin(acid_atp)]
-                        greta_LJ = greta_LJ[~greta_LJ.aj.isin(acid_atp)]
-                greta_LJ.append(make_pairs(fibril_pdb, atomic_mat_random_coil, fibril_atomtypes, parameters), sort = False, ignore_index = True)
-
+            greta_LJ = make_idp_epsilon(atomic_mat_plainMD, atomic_mat_random_coil, parameters)
         else:
-            print('ERRORONE')
-            exit()
+            atomic_mat_random_coil = random_coil_mdmat(parameters)
+            greta_LJ = make_pairs(native_pdb, atomic_mat_random_coil, native_atomtypes, parameters)
+            if parameters['acid_ff'] == True and acid_atp !=0:
+                    greta_LJ = greta_LJ[~greta_LJ.ai.isin(acid_atp)]
+                    greta_LJ = greta_LJ[~greta_LJ.aj.isin(acid_atp)]
 
+    elif parameters['greta_to_keep'] == 'merge':
+        if idp == True:
+            atomic_mat_plainMD = plainMD_mdmat(parameters)
+            atomic_mat_random_coil = random_coil_mdmat(parameters)
+            greta_LJ = make_idp_epsilon(atomic_mat_plainMD, atomic_mat_random_coil, parameters)
+            greta_LJ = greta_LJ.append(make_pairs(fibril_pdb, atomic_mat_random_coil, fibril_atomtypes, parameters), sort = False, ignore_index = True)
+        else:
+            atomic_mat_random_coil = random_coil_mdmat(parameters)
+            greta_LJ = make_pairs(native_pdb, atomic_mat_random_coil, native_atomtypes, parameters)
+            if parameters['acid_ff'] == True and acid_atp !=0:
+                    greta_LJ = greta_LJ[~greta_LJ.ai.isin(acid_atp)]
+                    greta_LJ = greta_LJ[~greta_LJ.aj.isin(acid_atp)]
+            greta_LJ = greta_LJ.append(make_pairs(fibril_pdb, atomic_mat_random_coil, fibril_atomtypes, parameters), sort = False, ignore_index = True)
+
+    else:
+        print("I dont' understand --build-from=",parameters['greta_to_keep'])
+        exit()
+
+    if parameters['greta_to_keep'] != 'rc':
         print('- Merging LJ interactions')
         greta_ffnb = merge_GRETA(greta_LJ, parameters)
         if parameters['N_terminal'] == True:
