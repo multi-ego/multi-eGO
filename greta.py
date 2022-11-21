4cafeb900a1b1ace8988955b9f853e1e75b7c30b
Carlo Camilloni <carlo.camilloni@gmail.com>
2022-11-21 23:49:24 +0100

    new dih set

--- greta.py
+++ greta.py
@@ -1060,7 +1060,7 @@ def make_pairs_exclusion_topology(ego_topology, bond_tuple, type_c12_dict, param
     sidechain_cb = atnum_type_top.loc[atnum_type_top['atom'] == 'CB']
     pro_cd = atnum_type_top.loc[(atnum_type_top['atom'] == 'CD')&(atnum_type_top['residue'] == 'PRO')]
 
-    # For proline CD take the CB and the N of the previous residue and save in a pairs tuple
+    # For proline CD take the CB, N of the previous residue and save in a pairs tuple
     # CB-1-CD is related to the extended region of the ramachandran
     # N-1-CD is related to the alpha region of the ramachandran (not used)
     pairs_14_ai, pairs_14_aj, pairs_14_c6, pairs_14_c12 = [], [], [], []
@@ -1125,6 +1125,24 @@ def make_pairs_exclusion_topology(ego_topology, bond_tuple, type_c12_dict, param
             pairs_14_aj.append(line_sidechain_cb['atom_number'])
             pairs_14_c6.append(0.0)
             pairs_14_c12.append(0.65*np.sqrt(line_sidechain_cb['c12']*line_backbone_nitrogen['c12']))
+
+    pairs_14 = pd.DataFrame(columns=['ai', 'aj', 'func', 'c6', 'c12'])
+    pairs_14['ai'] = pairs_14_ai
+    pairs_14['aj'] = pairs_14_aj
+    pairs_14['func'] = 1
+    pairs_14['c6'] = pairs_14_c6
+    pairs_14['c12'] = pairs_14_c12
+    pairs = pd.concat([pairs,pairs_14], axis=0, sort=False, ignore_index=True)
+
+    # For each backbone nitrogen take the N of the next residue and save in a pairs tuple
+    pairs_14_ai, pairs_14_aj, pairs_14_c6, pairs_14_c12 = [], [], [], []
+    for index, line_backbone_nitrogen in backbone_nitrogen.iterrows():
+        line_next_n = backbone_nitrogen.loc[backbone_nitrogen['residue_number'] == (line_backbone_nitrogen['residue_number']+1)].squeeze(axis=None)
+        if not line_next_n.empty:
+            pairs_14_ai.append(line_backbone_nitrogen['atom_number'])
+            pairs_14_aj.append(line_next_n['atom_number'])
+            pairs_14_c6.append(0.0)
+            pairs_14_c12.append(3.e-7)
 
     pairs_14 = pd.DataFrame(columns=['ai', 'aj', 'func', 'c6', 'c12'])
     pairs_14['ai'] = pairs_14_ai
