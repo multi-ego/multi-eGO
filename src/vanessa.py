b1b992f89d6fac230784bbf809814f5d7bf7ac08
Fran Bacic Toplek <fran0801@hotmail.de>
2023-05-25 14:09:15 +0200

    Mini changes after merging Bruno's stuff

--- src/vanessa.py
+++ src/vanessa.py
@@ -35,7 +35,7 @@ def initialize_ensemble_topology(topology, simulation):
                        'altloc', 'join', 'irotat', 'rmin', 'rmin_14', 'epsilon_14', 'tree']
     ensemble_topology_dataframe, new_number, col_molecule, new_resnum, ensemble_molecules_idx_sbtype_dictionary, temp_number_c12_dict = pd.DataFrame(), [], [], [], {}, {}
 
-    molecule_type_dict={}
+    molecule_type_dict = {}
     # I needed to add this for loop as by creating the topology dataframe by looping over molecules, the c12 information is lost
     for atom in topology.atoms:    
         temp_number_c12_dict[str(atom.idx+1)] = atom.epsilon*4.184
@@ -154,7 +154,6 @@ def get_pairs(topology):
   
 
 def assign_molecule_type(molecule_type_dict, molecule_name, molecule_topology):
-    #molecule_type_dict = {}
     first_aminoacid = molecule_topology.residues[0].name
 
     if first_aminoacid in aminoacids_list:
