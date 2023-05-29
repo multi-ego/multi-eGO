8b329ab9533d19ef69f9c3732092a665a7421f91
Fran Bacic Toplek <fran0801@hotmail.de>
2023-05-29 11:09:42 +0200

    Added comments and fixed dangling file pointers

--- multiego/io.py
+++ multiego/io.py
@@ -5,7 +5,17 @@ import os
 
 def read_molecular_contacts(path):
     '''
-    This function add column headers and add a column based whether the contacts are intramolecular or intermolecular
+    Reads intra-/intermat files to determine molecular contact statistics.
+
+    Parameters
+    ----------
+    path : str
+        The path to the file
+
+    Returns
+    -------
+    contact_matrix : pd.DataFrame
+        The content of the intra-/intermat file returned as a dataframe with columns ['molecule_number_ai', 'ai', 'molecule_number_aj', 'aj', 'distance_m', 'distance_NMR', 'distance_14', 'probability', 'cutoff']
     '''
 
     print('\t-', f"Reading {path}")
@@ -21,25 +31,26 @@ def read_molecular_contacts(path):
     return contact_matrix
 
 def write_nonbonded(topology_dataframe, lj_potential, parameters, output_folder):
-    # pd.set_option('display.colheader_justify', 'right')
+    '''
+    
+    '''
     header = make_header(vars(parameters))
-    file = open(f'{output_folder}/ffnonbonded.itp', 'w')
-    file.write(header)
-    file.write('\n[ atomtypes ]\n')
-    atomtypes = topology_dataframe[['sb_type', 'atomic_number', 'mass', 'charge', 'ptype', 'c6', 'c12']].copy()
-    atomtypes['c6'] = atomtypes['c6'].map(lambda x: '{:.6e}'.format(x))
-    atomtypes['c12'] = atomtypes['c12'].map(lambda x: '{:.6e}'.format(x))
-    file.write(dataframe_to_write(atomtypes))
-
-    if not lj_potential.empty:
-        file.write("\n\n[ nonbond_params ]\n")
-        lj_potential['c6'] = lj_potential['c6'].map(lambda x: '{:.6e}'.format(x))
-        lj_potential['c12'] = lj_potential['c12'].map(lambda x: '{:.6e}'.format(x))
-        lj_potential.insert(5, ';', ';')
-        lj_potential.drop(columns= ['molecule_name_ai', 'molecule_name_aj'], inplace=True)
-        file.write(dataframe_to_write(lj_potential))
-
-    file.close()
+    # pd.set_option('display.colheader_justify', 'right')
+    with open(f'{output_folder}/ffnonbonded.itp', 'w') as file:
+        file.write(header)
+        file.write('\n[ atomtypes ]\n')
+        atomtypes = topology_dataframe[['sb_type', 'atomic_number', 'mass', 'charge', 'ptype', 'c6', 'c12']].copy()
+        atomtypes['c6'] = atomtypes['c6'].map(lambda x: '{:.6e}'.format(x))
+        atomtypes['c12'] = atomtypes['c12'].map(lambda x: '{:.6e}'.format(x))
+        file.write(dataframe_to_write(atomtypes))
+    
+        if not lj_potential.empty:
+            file.write("\n\n[ nonbond_params ]\n")
+            lj_potential['c6'] = lj_potential['c6'].map(lambda x: '{:.6e}'.format(x))
+            lj_potential['c12'] = lj_potential['c12'].map(lambda x: '{:.6e}'.format(x))
+            lj_potential.insert(5, ';', ';')
+            lj_potential.drop(columns= ['molecule_name_ai', 'molecule_name_aj'], inplace=True)
+            file.write(dataframe_to_write(lj_potential))
 
 def write_model(meGO_ensemble, meGO_LJ_potential, meGO_LJ_14, parameters, output_dir):
     print('- Writing Multi-eGO model')
@@ -66,6 +77,18 @@ def write_model(meGO_ensemble, meGO_LJ_potential, meGO_LJ_14, parameters, output
     print('\nNessuno è più basito, nessuno è più sorpreso. Ognuno di voi ha capito tutto.\nCarlo is happy!\t\^o^/\n')
 
 def dataframe_to_write(df):
+    '''
+    Returns a stringified and formated dataframe and a message if the dataframe is empty.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        The input dataframe
+
+    Returns
+    -------
+    The stringified dataframe  
+    '''
     if df.empty:
         # TODO insert and improve the following warning
         print('A topology parameter is empty. Check the reference topology.')
@@ -95,57 +118,57 @@ def make_header(parameters):
 def write_topology(topology_dataframe, molecule_type_dict, bonded_interactions_dict, lj_14, parameters, output_folder):
     molecule_footer = []
     header = make_header(vars(parameters))
-    file = open(f'{output_folder}/topol_GRETA.top', 'w')
-    header += '''
+    with open(f'{output_folder}/topol_GRETA.top', 'w') as file:
+        header += '''
 ; Include forcefield parameters
 #include "multi-ego-basic.ff/forcefield.itp"
 '''
 
-    file.write(header)
-    for molecule, bonded_interactions in bonded_interactions_dict.items():
-        # TODO here I defined an empty exclusion. In the case we are not reading a protein topology, the exclusion part is not read and needed to be added in someway.
-        # Hence, an empty exclusion gives error. Here I define an empty variable so it does not gets stuck
-        exclusions = pd.DataFrame(columns=['ai', 'aj'])
-        # TODO here only proteins have custom pairs and exclusions. Nucleic acids and others will use the one in topol.top used as reference
-        # if molecule_type_dict[molecule] == 'protein':
-        pairs = lj_14[molecule]
-        pairs.insert(5, ';', ';')
-        pairs['c6'] = pairs["c6"].map(lambda x: '{:.6e}'.format(x))
-        pairs['c12'] = pairs["c12"].map(lambda x: '{:.6e}'.format(x))
-        bonded_interactions_dict[molecule]['pairs'] = pairs
-        exclusions = pairs[['ai', 'aj']].copy()
+        file.write(header)
+        for molecule, bonded_interactions in bonded_interactions_dict.items():
+            # TODO here I defined an empty exclusion. In the case we are not reading a protein topology, the exclusion part is not read and needed to be added in someway.
+            # Hence, an empty exclusion gives error. Here I define an empty variable so it does not gets stuck
+            exclusions = pd.DataFrame(columns=['ai', 'aj'])
+            # TODO here only proteins have custom pairs and exclusions. Nucleic acids and others will use the one in topol.top used as reference
+            # if molecule_type_dict[molecule] == 'protein':
+            pairs = lj_14[molecule]
+            pairs.insert(5, ';', ';')
+            pairs['c6'] = pairs["c6"].map(lambda x: '{:.6e}'.format(x))
+            pairs['c12'] = pairs["c12"].map(lambda x: '{:.6e}'.format(x))
+            bonded_interactions_dict[molecule]['pairs'] = pairs
+            exclusions = pairs[['ai', 'aj']].copy()
 
-        molecule_footer.append(molecule)
-        molecule_header = f'''
+            molecule_footer.append(molecule)
+            molecule_header = f'''
 [ moleculetype ]
 ; Name\tnrexcl
 {molecule}\t\t\t3
 
 '''
 
-        file.write(molecule_header)
-        file.write('[ atoms ]\n')
-        atom_selection_dataframe = topology_dataframe.loc[topology_dataframe['molecule_name'] == molecule][[
+            file.write(molecule_header)
+            file.write('[ atoms ]\n')
+            atom_selection_dataframe = topology_dataframe.loc[topology_dataframe['molecule_name'] == molecule][[
             'number', 'sb_type', 'resnum', 'resname', 'name', 'cgnr']].copy()
-        file.write(f'{dataframe_to_write(atom_selection_dataframe)}\n\n')
-        # Here are written bonds, angles, dihedrals and impropers
-        for bonded_type, interactions in bonded_interactions.items():
-            if interactions.empty:
-                continue
-            else:
-                if bonded_type == 'impropers':
-                    file.write(f'[ dihedrals ]\n')
+            file.write(f'{dataframe_to_write(atom_selection_dataframe)}\n\n')
+            # Here are written bonds, angles, dihedrals and impropers
+            for bonded_type, interactions in bonded_interactions.items():
+                if interactions.empty:
+                    continue
                 else:
-                    file.write(f'[ {bonded_type} ]\n')
-                file.write(dataframe_to_write(interactions))
-                file.write('\n\n')
-        # Here are written pairs and exclusions
-        # file.write(f'[ pairs ]\n')
-        # file.write(dataframe_to_write(pairs))
-        file.write(f'[ exclusions ]\n')
-        file.write(dataframe_to_write(exclusions))
+                    if bonded_type == 'impropers':
+                        file.write(f'[ dihedrals ]\n')
+                    else:
+                        file.write(f'[ {bonded_type} ]\n')
+                    file.write(dataframe_to_write(interactions))
+                    file.write('\n\n')
+            # Here are written pairs and exclusions
+            # file.write(f'[ pairs ]\n')
+            # file.write(dataframe_to_write(pairs))
+            file.write(f'[ exclusions ]\n')
+            file.write(dataframe_to_write(exclusions))
 
-    footer = f'''
+        footer = f'''
 
 ; Include Position restraint file
 #ifdef POSRES
@@ -159,11 +182,9 @@ def write_topology(topology_dataframe, molecule_type_dict, bonded_interactions_d
 ; Compound #mols
 '''
 
-    file.write(footer)
-    for molecule in molecule_footer:
-        file.write(f'{molecule}\t\t\t1\n')
-
-    file.close()
+        file.write(footer)
+        for molecule in molecule_footer:
+            file.write(f'{molecule}\t\t\t1\n')
 
 def get_name(parameters):
     if parameters.egos =="rc":
