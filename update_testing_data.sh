0a74f2107e8dc3edd1c9dc025db50ca9bb21520f
Fran Bacic Toplek <fran0801@hotmail.de>
2023-06-01 18:02:41 +0200

    Added automatic list-based test generation and updating

--- update_testing_data.sh
+++ update_testing_data.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 ROOT=$(pwd)
+TEST_CASES="test/test_cases.txt"
 
 # TODO make dependent on set parameters and loop etc
 [ -d "${ROOT}/inputs/gpref" ] && rm -rf ${ROOT}/inputs/gpref
@@ -7,13 +8,19 @@ ROOT=$(pwd)
 [ -d "${ROOT}/inputs/ttrref" ] && rm -rf ${ROOT}/inputs/ttrref
 cp -r test/test_inputs/* inputs
 
-echo "Generating data for examples..."
-python multiego.py --system=gpref --egos rc > /dev/null
-python multiego.py --system=gpref --egos production --epsilon 0.35 --train_from md_ensemble > /dev/null
-python multiego.py --system=abetaref --egos production --epsilon 0.35 --train_from native_MD > /dev/null
-python multiego.py --system=ttrref --egos production --epsilon 0.225 --train_from native_MD fibril_MD --check_with fibril_check --inter_epsilon 0.3 > /dev/null
-
-echo "Finished generating\nMoving data to reference output directory"
-
+echo "Deleting current test_outputs"
 rm -rf test/test_outputs/*
-mv outputs/gpref_rc outputs/gpref_production_e0.35_0.35 outputs/abetaref_production_e0.35_0.35 outputs/ttrref_production_e0.225_0.3 test/test_outputs
+
+echo "Generating data for examples from ${ROOT}/${TEST_CASES}..."
+# while read case; do echo $case ; done < ${ROOT}/${TEST_CASES}
+while read case; do
+    out_dir=$(python multiego.py $case | grep outputs/)
+    mv $out_dir test/test_outputs
+done < ${ROOT}/${TEST_CASES}
+# python multiego.py < "$(cat ${ROOT}/${TEST_CASES} | xargs -0 -l -d \\n echo
+# python multiego.py --system=gpref --egos rc > /dev/null
+# python multiego.py --system=gpref --egos production --epsilon 0.35 --train_from md_ensemble > /dev/null
+# python multiego.py --system=abetaref --egos production --epsilon 0.35 --train_from native_MD > /dev/null
+# python multiego.py --system=ttrref --egos production --epsilon 0.225 --train_from native_MD fibril_MD --check_with fibril_check --inter_epsilon 0.3 > /dev/null
+
+echo "Finished generating test outputs!"
