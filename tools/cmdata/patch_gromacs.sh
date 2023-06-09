65cecd191c73d6c1843a16fa3acd1cda6b9202a6
Fran Bacic Toplek <fran0801@hotmail.de>
2023-06-09 16:21:59 +0200

    Added patch script

---
+++ tools/cmdata/patch_gromacs.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+CURRENT=$(pwd)
+GROMACS_ROOT_DIR=$1
+
+[[ $# -eq 0 ]] && [[ ! -d $GROMACS_ROOT_DIR ]] &&  echo "Please supply a valid gromacs root directory as a command-line argument" && exit 0
+
+diff -aur ${GROMACS_ROOT_DIR}/src/gromacs/trajectoryanalysis/modules.cpp modules.cpp > patch.diff
+cp cmdata.cpp cmdata.h ${GROMACS_ROOT_DIR}/src/gromacs/trajectoryanalysis/modules/
+
+patch -d/ -p0 < patch.diff
