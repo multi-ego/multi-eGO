fix masking

multiego/util/masking.py
@@ -25,7 +25,7 @@ def create_linearized_mask(set1, set2, types, symmetrize=False, inner_op=lambda 
     return mask
 
 def map_c12_mask(types, mask, standard_c12_dict, special_c12_dict):
-    translator = lambda types, c12s_dict: np.vectorize(my_dict.__getitem__)(a)
+    translator = lambda types, c12s_dict: np.vectorize(c12s_dict.__getitem__)(types)
     standard_c12 = np.where(np.logical_not(mask), translator(types, standard_c12_dict), 0.0)
     special_c12 = np.where(mask, translator(types, special_c12_dict), 0.0)
 