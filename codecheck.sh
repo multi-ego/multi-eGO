99148199939d5c9afbc3204a57557c4b97e2b46b
Carlo Camilloni <carlo.camilloni@gmail.com>
2023-12-07 16:38:17 +0100

    codechecker

--- codecheck.sh
+++ codecheck.sh
@@ -8,7 +8,18 @@ echo "you can install it as pip install git+https://github.com/psf/black"
 fi
 flake8 . --count --extend-ignore E501,W605,E731,E402,E711 --show-source --statistics
 if [ $? -eq 1 ]; then
-echo "Fix the remaining issue before pushing your code"
-else
-echo "Your code is ready to be pushed"
+flak="Fix the remaining formatting issue before pushing your code"
+fi
+echo "Running the regtests"
+python test/run_tests.py >& /dev/null
+if [ $? -eq 1 ]; then
+echo "Regtests failing, you should not push your code"
+echo $flak
+elif [ $? -eq 0 ]; then
+  if [ -z "$flak" ]; then
+    echo "Ready to push"
+  else
+    echo "Regtest passed"
+    echo $flak
+  fi
 fi
