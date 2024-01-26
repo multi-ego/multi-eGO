cd25a1d02362236dbd635f8c9c3809af56ba7ffc
Carlo Camilloni <carlo.camilloni@gmail.com>
2024-01-26 19:19:07 +0100

    fix regtest

--- test/run_make_mat.sh
+++ test/run_make_mat.sh
@@ -4,7 +4,7 @@ set -e
 set -o pipefail
 
 tar -zxf test_inputs/make_mat_ttr/hh.tgz -C test_inputs/make_mat_ttr/
-python ../tools/make_mat/make_mat.py --histo test_inputs/make_mat_ttr/histo.tgz --target_top test_inputs/make_mat_ttr/topol_md.top --mego_top test_inputs/make_mat_ttr/topol_ref.top --cutoff 0.75 --inter --out  test_inputs/make_mat_ttr/ --tar
+python ../tools/make_mat/make_mat.py --histo test_inputs/make_mat_ttr/hh.tgz --target_top test_inputs/make_mat_ttr/topol_md.top --mego_top test_inputs/make_mat_ttr/topol_ref.top --cutoff 0.75 --inter --out  test_inputs/make_mat_ttr/ --tar
 python ../tools/make_mat/make_mat.py --histo test_inputs/make_mat_ttr/histo --target_top test_inputs/make_mat_ttr/topol_md.top --mego_top test_inputs/make_mat_ttr/topol_ref.top --cutoff 0.75 --out  test_inputs/make_mat_ttr/
 diff <(gzip -dc test_inputs/make_mat_ttr/intramat_1_1.ndx.gz) <(gzip -dc test_outputs/make_mat_ttr/intramat_1_1.ndx.gz)
 diff <(gzip -dc test_inputs/make_mat_ttr/intermat_1_1.ndx.gz) <(gzip -dc test_outputs/make_mat_ttr/intermat_1_1.ndx.gz)
