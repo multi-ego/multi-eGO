f6585494426b43ec775a5ec2e2a71ca7ce251648
Carlo Camilloni <carlo.camilloni@gmail.com>
2024-03-21 17:40:20 +0100

    Revert "New distances"

--- test/run_cmdata.sh
+++ test/run_cmdata.sh
@@ -2,7 +2,7 @@ set -e
 set -o pipefail
 mkdir test_inputs/cmdata/histo
 cd test_inputs/cmdata/histo
-cmdata -f ../traj.xtc -s ../protein.tpr --mode=intra
+echo "0" | gmx cmdata -f ../traj.xtc -s ../protein.tpr -sym ../aa_sym -mode intra
 cd ../../../
 tar -zxf test_outputs/cmdata/hh.tgz -C test_outputs/cmdata/
 for i in `ls -1 test_inputs/cmdata/histo`; do
