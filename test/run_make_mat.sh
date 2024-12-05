0a004eca64409ed1192b180dc4f4e0d23ce21ae0
Carlo Camilloni <carlo.camilloni@gmail.com>
2024-12-05 23:47:09 +0100

    new make_mat test

--- test/run_make_mat.sh
+++ test/run_make_mat.sh
@@ -12,3 +12,11 @@ tar -zxf test_inputs/make_mat_popc/hh.tgz -C test_inputs/make_mat_popc/
 python ../tools/make_mat/make_mat.py --histo test_inputs/make_mat_popc/histo --target_top test_inputs/make_mat_popc/topol_md.top --mego_top test_inputs/make_mat_popc/topol_ref.top --cutoff 0.75 --mode intra --out  test_inputs/make_mat_popc/ 
 diff <(gzip -dc test_inputs/make_mat_popc/intramat_1_1.ndx.gz) <(gzip -dc test_outputs/make_mat_popc/intramat_1_1.ndx.gz)
 
+tar -zxf test_inputs/make_mat_het_trim/hh.tgz -C test_inputs/make_mat_het_trim/
+python ../tools/make_mat/make_mat.py --histo test_inputs/make_mat_het_trim/histo --target_top test_inputs/make_mat_het_trim/topol_mego.top --mego_top test_inputs/make_mat_het_trim/topol.top --cutoff 0.75 --out  test_inputs/make_mat_het_trim/ 
+diff <(gzip -dc test_inputs/make_mat_het_trim/intramat_1_1.ndx.gz) <(gzip -dc test_outputs/make_mat_het_trim/intramat_1_1.ndx.gz)
+diff <(gzip -dc test_inputs/make_mat_het_trim/intramat_2_2.ndx.gz) <(gzip -dc test_outputs/make_mat_het_trim/intramat_2_2.ndx.gz)
+diff <(gzip -dc test_inputs/make_mat_het_trim/intramat_3_3.ndx.gz) <(gzip -dc test_outputs/make_mat_het_trim/intramat_3_3.ndx.gz)
+diff <(gzip -dc test_inputs/make_mat_het_trim/intermat_1_2.ndx.gz) <(gzip -dc test_outputs/make_mat_het_trim/intermat_1_2.ndx.gz)
+diff <(gzip -dc test_inputs/make_mat_het_trim/intermat_1_3.ndx.gz) <(gzip -dc test_outputs/make_mat_het_trim/intermat_1_3.ndx.gz)
+diff <(gzip -dc test_inputs/make_mat_het_trim/intermat_2_3.ndx.gz) <(gzip -dc test_outputs/make_mat_het_trim/intermat_2_3.ndx.gz)
