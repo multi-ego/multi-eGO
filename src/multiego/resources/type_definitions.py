7a5c5bc4913c71fd230bfb9ea31905e9599004f9
Carlo Camilloni <carlo.camilloni@gmail.com>
2025-07-17 11:32:36 +0200

    6 bond rule and new 1-4

--- src/multiego/resources/type_definitions.py
+++ src/multiego/resources/type_definitions.py
@@ -2,10 +2,12 @@ import pandas as pd
 import json
 import sys
 
-mg_OO_c12_rep = 3e-6
-mg_HH_c12_rep = 1.2e-8
-mg_ON_c12_rep = 3e-6
-mg_NN_c12_rep = 2.5e-5
+mg_OO_c12_rep = 1.5e-6
+#mg_HH_c12_rep = 1.2e-8
+mg_HH_c12_rep = 3e-9
+mg_ON_c12_rep = 1.5e-6
+#mg_NN_c12_rep = 2.5e-5
+mg_NN_c12_rep = 5e-6
 mg_HO_sigma = 0.169500
 mg_eps_ch3 = 0.13
 mg_eps_ch2 = 0.10
